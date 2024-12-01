9dc58431a03f6ef6234c29d86191e624dff8c990
Cannizzaro_reaction <157903250+Cannizzaro-reaction@users.noreply.github.com>
2024-12-01 17:26:21 +0800

    Update __init__.py

--- web/models/__init__.py
+++ web/models/__init__.py
@@ -4,3 +4,10 @@ db = SQLAlchemy()
 # import models
 from .go_info_model import GoInfo
 from .go_interaction_model import GoInteraction
+from .Ecoli_interaction_score_model import EcoliInteractionScore
+from .Scer_interaction_score_model import ScerInteractionScore
+from .Ecoli_validation_model import EcoliValidation
+from .Scer_validation_model import ScerValidation
+from .species_protein_model import SpeciesProtein
+from .Ecoli_protein_model import EcoliPS, EcoliSS, EcoliTS
+from .Scer_protein_model import ScerPS, ScerSS, ScerTS

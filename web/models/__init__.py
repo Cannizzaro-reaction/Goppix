3b75971e7dbdb5e72355f5f8421a535c561ed262
Cannizzaro_reaction <157903250+Cannizzaro-reaction@users.noreply.github.com>
2025-01-18 15:52:57 +0800

    fix an error in __init__.py

--- web/models/__init__.py
+++ web/models/__init__.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # import models
-from .go_model import GoInteraction, GoInfo, EcoliProtGO, ScerProtGO
+from .go_model import GoInteraction, GoBasic, GoDetail, EcoliProtGO, ScerProtGO
 from .Ecoli_interaction_score_model import EcoliInteractionScore
 from .Scer_interaction_score_model import ScerInteractionScore
 from .Ecoli_validation_model import EcoliValidation
